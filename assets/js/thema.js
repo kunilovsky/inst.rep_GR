b916f52f133e9f68259ff326dfb06e3bff2785c0
kunilovsky <kunilovsky@gmail.com>
2023-01-26 19:38:42 +0300

    Thema refactor

--- assets/js/thema.js
+++ assets/js/thema.js
@@ -3,8 +3,8 @@ const theme = window.localStorage.getItem('theme');
 
 if (theme === 'dark') {
   document.body.classList.add('dark');
-  toggle.classList.add('thema__action-1');
-} else toggle.classList.add('thema__action-2');
+  toggle.classList.add('icon__light');
+} else toggle.classList.add('icon__dark');
 
 toggle.addEventListener('click', () => {
   document.body.classList.toggle('dark');
@@ -13,14 +13,11 @@ toggle.addEventListener('click', () => {
   } else {
     window.localStorage.setItem('theme', 'dark');
   }
-});
-
-toggle.addEventListener('click', () => {
-  if (toggle.classList.contains('thema__action-1')) {
-    toggle.classList.remove('thema__action-1');
-    toggle.classList.add('thema__action-2');
+  if (toggle.classList.contains('icon__light')) {
+    toggle.classList.remove('icon__light');
+    toggle.classList.add('icon__dark');
   } else {
-    toggle.classList.remove('thema__action-2');
-    toggle.classList.add('thema__action-1');
+    toggle.classList.remove('icon__dark');
+    toggle.classList.add('icon__light');
   }
 });
