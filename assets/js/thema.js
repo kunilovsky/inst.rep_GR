8772631c900fbb256f5121a5d833c65938a8996b
kunilovsky <kunilovsky@gmail.com>
2023-01-30 18:14:20 +0300

    Thema icon menu add

--- assets/js/thema.js
+++ assets/js/thema.js
@@ -1,13 +1,16 @@
 const toggle = document.getElementById('toggle');
 const theme = window.localStorage.getItem('theme');
+const burger = document.getElementById('burger');
 
 if (theme === 'dark') {
   document.body.classList.add('dark');
   toggle.classList.add('icon__light');
+  burger.classList.add('navbar-toggler-icon-white')
 } else toggle.classList.add('icon__dark');
 
 toggle.addEventListener('click', () => {
   document.body.classList.toggle('dark');
+  burger.classList.toggle('navbar-toggler-icon-white');
   if (theme === 'dark') {
     window.localStorage.setItem('theme', 'light');
   } else {
